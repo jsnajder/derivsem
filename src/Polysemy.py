596ded59c895eb0477128baa099accf1cc73c7e9
Jan Snajder <jan.snajder@fer.hr>
2016-06-17 23:26:11 +1000

    MRR added

--- src/Polysemy.py
+++ src/Polysemy.py
@@ -138,6 +138,7 @@ def predict(self, base, verbose=False):
     if self.lexfun == None:
         raise NameError('Error: Model has not yet been trained')
     composed_space = self.lexfun.compose([('dummy', base, 'derived')], self.space)
+    print composed_space
     return composed_space.get_row('derived')
 
 ##############################################################################
