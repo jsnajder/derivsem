2e4bb73f93c514fa1a71ab275ce2ac1d5dc3f04b
Jan Snajder <jan.snajder@fer.hr>
2016-06-20 07:06:20 +0200

    Add evaluate script

--- scripts/run-evaluate.sh
+++ scripts/run-evaluate.sh
@@ -2,7 +2,7 @@
 
 for model in "baseline" "add" "lexfun"; do
   for space in "cbow-w2" "cbow-w5" "cbow-w10"; do
-    echo "nohup python -u ../src/evaluate.py ../data/pairs-all.txt $model $space ../results/ > ../results/$model-$space.txt &"
+    nohup python -u ../src/evaluate.py ../data/pairs-all.txt $model $space ../results/ > ../results/$model-$space.txt &
   done;
 done;
 
