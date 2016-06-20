de1c12c73e2c8ee5ff934d08d92e504d8453fd10
Jan Snajder <jan.snajder@fer.hr>
2016-06-21 08:05:26 +1000

    Fix output bug

--- src/evaluate.py
+++ src/evaluate.py
@@ -37,7 +37,7 @@ def evaluate(partitioned_pairs_df, models_dict, patterns=None, verbose=False):
             model.fit(train_pairs, verbose=verbose)
             _, target_pos = pattern_pos(pattern)
             scores_test = reciprocal_rank_scores(model, get_word_pairs(pairs_df), pos=target_pos, verbose=verbose)
-            df = pairs_df[['word1', 'word2', 'partition']]
+            df = pairs_df[['word1', 'word2', 'partition']].copy()
             df.loc[:, 'baseline-cbow-w5'] = pd.Series(scores_test, index=df.index)
             dfs.append(df)
         return pd.concat(dfs)
